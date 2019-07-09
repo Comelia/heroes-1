e6a9ddb8b65e2ba00d4fd24539d995ac30bf2b13
Comelia <1511867031@qq.com>
2019-07-09 08:53:03 +0800

    英雄列表 数据请求

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import App from './App.vue'
 
 import router from './router.js'
 
+import axios from 'axios'
+// 配置axios的基地址, 调用请求方法的时候get(url),会自动把请求方法的路径拼接到及地址后面
+axios.defaults.baseURL = 'http://localhost:3000'
+// 让所有 Vue实例都具有一个属性axios , 给Vue构造函数的原型增加成员
+Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
 
