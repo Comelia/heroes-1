1edebd360b6741203b6003c8f9c0f19f2932ae58
Comelia <1511867031@qq.com>
2019-07-09 08:13:58 +0800

    配置路由 完成列表路由切换

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import './assets/css/index.css'
 
 import App from './App.vue'
 
+import router from './router.js'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 配置实例选项 router
+  router,
   render: h => h(App),
 }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,15 +1,25 @@
 // 导入vue
-import vue from 'vue'
+import Vue from 'vue'
 
 // 导入 vue-router
 import VueRouter from 'vue-router';
+Vue.use(VueRouter)
+// 导入组件
+import HeroList from './views/hero/HeroList.vue'
+import WeqponList from './views/weapon/WeqponList.vue'
+import EquipList from './views/equip/EquipList.vue'
 
 
 // 创建路由对象 
 const router = new VueRouter({
+    // 当浏览器地址栏中的路由地址 #/hero/add 包含router-link生成的标签的herf地址, 添加类样式
+    linkActiveClass: 'active',
     // 配置路由规则
     routes: [
-        { name: '' }
+        { path: '/' , redirect: { name: 'hero' } },
+        { name: 'hero' ,path: '/hero' , component: HeroList },
+        { name: 'weapon' ,path: '/weapon' , component: WeqponList },
+        { name: 'equip' ,path: '/equip' , component: EquipList },
     ]
 
 
